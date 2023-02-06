51150ddd9dba7fedea95718c6d463e7c248dcf20
Jorge <jcorvil600@g.educaand.es>
2023-02-06 11:48:32 +0100

    Matrices

--- src/Practica4/Ej2NumPalabras/Main.java
+++ src/Practica4/Ej2NumPalabras/Main.java
@@ -1,12 +1,14 @@
 package Practica4.Ej2NumPalabras;
 
+import java.util.Objects;
+
 public class Main {
 
     /*2.-Escribe un programa que sea capaz de contar el número de palabras diferentes que hay en un texto que
      se le pasa por argumento, sin tener en cuenta si están escritas en mayúsculas o minúsculas.*/
 
     public static void main(String[] args) {
-        String texto = "Hola que tal me llamo Jorge";
+        String texto = "Hola Manolito hola Pepito";
         System.out.println("La cadena " + "'" + texto + "'" + " tiene " + contadorPalabras(texto) + " palabras");
     }
 
@@ -17,6 +19,16 @@ public class Main {
         for (int i = 0; i < separador.length; i++) {
             contador++;
         }
+
+
+        //TODO revisar bucle. No cuenta ninguna palabra repetida en lugar de contarla solo una vez
+        for (int i = 0; i < separador.length; i++) {
+            for (int j = 0; j < separador.length; j++) {
+                if (separador[i].equalsIgnoreCase(separador[j]) && !Objects.equals(separador[i], separador[j]))
+                    contador--;
+            }
+        }
+        
 
         return contador;
     }
